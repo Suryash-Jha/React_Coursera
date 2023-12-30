29ba495ad2235de875020fabce14057258ab9d68
Suryash <joblesscoder2468@gmail.com>
2023-12-30 10:23:06 +0530

    Initial Stage, Installed using npm init react-app . , Tried using Variable in a JSX

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Its written in JSX
+function App() {
+	let title= "Hye Guys"
+  return (
+  	<>
+	  <h1>{title}</h1>
+	  </>
+  );
+}
+
+export default App;
