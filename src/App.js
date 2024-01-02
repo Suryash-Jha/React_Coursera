8e9a81408b3a4d25050d097d86fea99c8473d4e9
Suryash <joblesscoder2468@gmail.com>
2024-01-02 09:18:27 +0530

    Routes creating successful

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import LightToggler from './components/LightToggler';
 //Its written in JSX
 function App() {
 	let title= "Hye Guys"
   return (
-  	<>
-	  <Nav name="suryash" greet="afternoon" />
-	  </>
+	  <BrowserRouter>
+	  <Routes>
+	  	<Route path="light" element= {<LightToggler />} />
+	  	<Route index element= {<Nav />} />
+	  </Routes>
+	  </BrowserRouter>
   );
 }
 
---
+++ src/components/LightToggler.js
@@ -0,0 +1,12 @@
+function LightToggler(){
+	return (
+		<>
+		<h1> Light Toggler Component -> To be created later
+		</h1>
+		</>
+	)
+
+}
+
+
+export default LightToggler;
